1f1f1b939ab4500081224bf3f739fb1a5eae41ef
Andrew Smith <andrewchristophersmith@gmail.com>
2015-05-14 17:12:30 -0700

    Implemented statistical feedback as described in Ames 1990

--- lib/statfeed.rb
+++ lib/statfeed.rb
@@ -1,9 +1,97 @@
 class Statfeed
   VERSION = "1.0.0"
-  attr_accessor :weights
+  attr_accessor :weights, :choices, :randoms, :heterogeneities, :accents, :statistics, :size, :decisions, :options
  
-  def initialize size
-    @weights = Array.new(size, 1)
+  def initialize decisions, options, heterogeneity: 0.1, accent: 1.0
+    @decisions = decisions
+    @options = options
+
+    # This will eventually be the result of applying statistical feedback
+    @choices = Array.new(@decisions.size, nil)
+
+    # Vector of heterogeneity values for each decision
+    @heterogeneities = Array.new(decisions.size, heterogeneity)
+    # Vector of accent values for each decision
+    @accents = Array.new(decisions.size, accent)
+    # Vector of statistic for each option
+    @statistics = Array.new(options.size, 0.0)
+
+    # Fill matrix of random values
+    populate_randoms
+    # Fill matrix of weights
+    populate_weights
+  end
+
+  def populate_choices
+    @decisions.each_index do |decision|
+      vals = scheduling_values(decision)
+      @choices[decision] = sort_options(vals).first
+      increment_statistics decision, sort_option_indices(vals).first
+      normalize_statistics! decision
+    end
+    @choices
+  end
+
+  def populate_randoms
+    @randoms = Array.new @decisions.size do
+      Array.new @options.size do
+        rand
+      end
+    end
+  end
+
+  def populate_weights
+    @weights = Array.new @decisions.size do
+      Array.new @options.size do
+        1.0 / @options.size
+      end
+    end
+  end
+
+  def expected_increment decision, option
+    (@accents[decision] + (@heterogeneities[decision] * @randoms[decision][option])).to_f / @weights[decision][option]
+  end
+
+  def true_increment decision, option
+    @accents[decision].to_f / @weights[decision][option]
+  end
+
+  def normalization_value decision
+    @accents[decision].to_f / @weights[decision].inject(0.0, :+)
+  end
+
+  def scheduling_values decision
+    (0...@options.size).map do |m|
+      @statistics[m] + expected_increment(decision, m)
+    end
+  end
+
+  # Feed the scheduling values into this (step 2)
+  def sort_options vals
+    @options.zip(vals).sort_by(&:last).map(&:first)
+  end
+
+  # Feed the scheduling values into this (step 2)
+  def sort_option_indices vals
+    (0...@options.size).to_a.zip(vals).sort_by(&:last).map(&:first)
+  end
+
+  def increment_statistics decision, option
+    @statistics[option] += true_increment(decision, option)
+  end
+
+  def normalize_statistics decision
+    @statistics.each_with_index.map do |statistic, m|
+      if @weights[decision][m] > 0.0
+        statistic - normalization_value(decision)
+      else
+        statistic
+      end
+    end
+  end
+
+  def normalize_statistics! decision
+    @statistics = normalize_statistics decision
   end
 
   def sample
--- test/test_statfeed.rb
+++ test/test_statfeed.rb
@@ -2,6 +2,31 @@ require "minitest/autorun"
 require "statfeed"
 
 class TestStatfeed < Minitest::Test
+  def setup
+    @sf = Statfeed.new([0, 1, 2], %w(a b c))
+    # Need to control the "random" numbers in order to assert answers
+    @sf.randoms = [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]]
+  end
+
+  def test_true_increment
+    assert_in_delta 3.0, @sf.true_increment(0, 0)
+  end
+
+  def test_expected_increment
+    actual = @sf.expected_increment(0, 0)
+    assert_in_delta 3.03, actual
+  end
+
+  def test_scheduling_values
+    actual = @sf.scheduling_values(0)
+    assert_each_in_delta [3.3633, 3.3933, 3.4233], actual
+  end
+
+  def test_sort_options
+    actual = @sf.sort_options([0.3, 0.5, 0.4])
+    assert_each_equal %w(a c b), actual
+  end
+
   def test_n_decisions
     # Should have N possible decisions
   end
@@ -16,5 +41,29 @@ class TestStatfeed < Minitest::Test
 
   def test_a_sub_n
     # Accent (stress) associated with the decision n
+  end
+
+  def test_h_sub_n
+    # Heterogeneity associated with decision n
+  end
+
+  def test_w_sub_m_n
+    # Weight associated with option m during decision n
+  end
+
+  def test_u_sub_m_n
+    # Uniformly distributed random number for each option of each decision
+  end
+
+  def assert_each_in_delta exp, act, delta = 0.001
+    exp.zip(act).each do |e, a|
+      assert_in_delta e, a, delta
+    end
+  end
+
+  def assert_each_equal exp, act
+    exp.zip(act).each do |e, a|
+      assert_equal e, a
+    end
   end
 end
